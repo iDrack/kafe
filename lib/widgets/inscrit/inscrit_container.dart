5583aea26a376c19c4fa98e8ab500a934b9bcd4e
Thomas Dignoire <tdignoire@protonmail.com>
2025-04-11 14:47:45 +0200

    Ajout du widget NotFound pour gérer l'absence d'assemblages inscrits

--- lib/widgets/inscrit/inscrit_container.dart
+++ lib/widgets/inscrit/inscrit_container.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kafe/providers/assemblage_stream_provider.dart';
 import 'package:kafe/widgets/inscrit/assemblage_inscrit_card.dart';
 import 'package:kafe/widgets/lotti/loading_coffee_widget.dart';
+import 'package:kafe/widgets/lotti/not_found_widget.dart';
 
 import '../../models/assemblage.dart';
 import '../../providers/firebase_auth_provider.dart';
@@ -30,9 +31,7 @@ class InscritContainer extends HookConsumerWidget {
         if (snapshot.hasData) {
           final assemblages = snapshot.data!;
           if (assemblages.isEmpty) {
-            return Center(
-              child: Text("Aucun assemblage inscrit au prochain concours."),
-            );
+            return Center(child: NotFoundWidget(title: "Aucun assemblage inscrit au prochain concours.",));
           }
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 32.0),
